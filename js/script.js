d87eb7299fb05a54257ad99729d10a6853102da8
anthonbrooks <51774498+anthonbrooks@users.noreply.github.com>
2026-01-18 16:57:03 -0700

    Add rotation for minutes

--- js/script.js
+++ js/script.js
@@ -1,15 +1,20 @@
 const secondHand = document.querySelector('.second-hand');
 const hourHand = document.querySelector('.hour-hand');
+const minuteHand = document.querySelector('.min-hand');
 
 function setDate() {
     const now = new Date();
     const seconds = now.getSeconds();
     const hours = now.getHours();
+    const minutes = now.getMinutes();
+
     const secondsDegrees = ((seconds / 60) * 360) + 90;
     const hourDegrees = ((hours / 12) * 360 + 90);
+    const minuteDegrees = ((minutes / 60) * 360) + 90;
 
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
     hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+    minuteHand.style.transform = `rotate(${minuteDegrees}deg)`;
 
     console.log(now);
 }
